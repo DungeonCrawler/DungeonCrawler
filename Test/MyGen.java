b012ba446c78fc2d5324d2b90c5b38133a367c20
Dxier <smithjamiec@gmail.com>
2014-05-13 14:24:45 -0400

    Work on New Generation
    Similar to the DunGen algorithm.  Just started. More to come.

---
+++ Test/MyGen.java
@@ -0,0 +1,13 @@
+public class MyGen
+{
+    private final int tileEmpty=0;
+    private final int tileWall=1;
+    private final int tileFloor=2;
+    private final int tileDoor=3;
+    private final int tileUpStairs=4;
+    private final int tileDownStairs=5;
+    public MyGen()
+    {
+        
+    }
+}
