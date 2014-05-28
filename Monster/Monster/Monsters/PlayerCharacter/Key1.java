8863a2a1db9fbf11df051aa11c3413598e245a92
Drakhaol961 <Stevenrenn96@gmail.com>
2014-05-28 13:28:11 -0400

    New Key

--- Monster/Monster/Monsters/PlayerCharacter/Key1.java
+++ Monster/Monster/Monsters/PlayerCharacter/Key1.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Key1 extends JFrame implements KeyListener{
 
     JTextField KeyCodeT = new JTextField("Key Code:");//A Text Field that will display the key code.
-
+    private int count, Right_Key, Left_Key, Down_Key, Up_Key;
     public Key1(){
 
         KeyCodeT.addKeyListener(this);//Listens for key inputs in the text field
@@ -20,6 +20,7 @@ public class Key1 extends JFrame implements KeyListener{
 
         setVisible(true);//show the window on screen.    
 
+        count=0;
     }
 
     //Called when the key is pressed down.
@@ -28,34 +29,53 @@ public class Key1 extends JFrame implements KeyListener{
 
         //System.out.println("Key Pressed!!!");
 
-
         if(e.getKeyCode()==27) {//check if the Keycode is 27 which is esc
-
             JOptionPane.showMessageDialog(null,"Good  Bye");//display a good bye messege
-
             System.exit(0);//exit
-
         }
-
-        if(e.getKeyCode()==37){
-            //set direction left
-            System.out.println("LEFT");
-            //entity.move(3);
+        if(count<4){
+            if(count==0){
+                Left_Key=e.getKeyCode();
+                count++;
+                return;
+            }
+            if(count==1){
+                Right_Key=e.getKeyCode();
+                count++;
+                return;
+            }
+            if(count==2){
+                Down_Key=e.getKeyCode();
+                count++;
+                return;
+            }
+            if(count==3){
+                Up_Key=e.getKeyCode();
+                count++;
+                return;
+            }
         }
-        if(e.getKeyCode()==39){
-            //set direction right
-            System.out.println("RIGHT");
-            //entity.move(1);
-        }
-        if(e.getKeyCode()==40){
-            //set direction down
-            System.out.println("DOWN");
-            //entity.move(2);
-        }
-        if(e.getKeyCode()==38){
-            //set direction up
-            System.out.println("UP");
-            //entity.move(0);
+        else{
+            if(e.getKeyCode()==Left_Key){
+                //set direction left
+                System.out.println("LEFT");
+                //entity.move(3);
+            }
+            if(e.getKeyCode()==Right_Key){
+                //set direction right
+                System.out.println("RIGHT");
+                //entity.move(1);
+            }
+            if(e.getKeyCode()==Down_Key){
+                //set direction down
+                System.out.println("DOWN");
+                //entity.move(2);
+            }
+            if(e.getKeyCode()==Up_Key){
+                //set direction up
+                System.out.println("UP");
+                //entity.move(0);
+            }
         }
     }
 
@@ -67,16 +87,12 @@ public class Key1 extends JFrame implements KeyListener{
 
         KeyCodeT.setText("Key Code:" + e.getKeyCode());//displays the key code in the text box
 
-
     }
-
     //Called when a key is typed
-
     public void keyTyped(KeyEvent e){
-
     }
     public static void main(String[] args){
         Key1 key = new Key1();
     }
-     
+
 }
