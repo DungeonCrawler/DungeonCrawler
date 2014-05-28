8d2f2498e249dc0f6f607dbd52fdaa178cce8e02
Drakhaol961 <Stevenrenn96@gmail.com>
2014-05-28 13:44:18 -0400

    NEWNEWkey

--- Monster/Monster/Monsters/PlayerCharacter/Key1.java
+++ Monster/Monster/Monsters/PlayerCharacter/Key1.java
@@ -8,6 +8,15 @@ public class Key1 extends JFrame implements KeyListener{
 
     JTextField KeyCodeT = new JTextField("Key Code:");//A Text Field that will display the key code.
     private int count, Right_Key, Left_Key, Down_Key, Up_Key;
+    public final static int numpad1 = KeyEvent.VK_NUMPAD1;
+    public final static int numpad2 = KeyEvent.VK_NUMPAD2;
+    public final static int numpad3 = KeyEvent.VK_NUMPAD3;
+    public final static int numpad4 = KeyEvent.VK_NUMPAD4;
+    public final static int numpad5 = KeyEvent.VK_NUMPAD5;
+    public final static int numpad6 = KeyEvent.VK_NUMPAD6;
+    public final static int numpad7 = KeyEvent.VK_NUMPAD7;
+    public final static int numpad8 = KeyEvent.VK_NUMPAD8;
+    public final static int numpad9 = KeyEvent.VK_NUMPAD9;
     public Key1(){
 
         KeyCodeT.addKeyListener(this);//Listens for key inputs in the text field
@@ -21,6 +30,7 @@ public class Key1 extends JFrame implements KeyListener{
         setVisible(true);//show the window on screen.    
 
         count=0;
+
     }
 
     //Called when the key is pressed down.
@@ -33,64 +43,80 @@ public class Key1 extends JFrame implements KeyListener{
             JOptionPane.showMessageDialog(null,"Good  Bye");//display a good bye messege
             System.exit(0);//exit
         }
-        if(count<4){
-            if(count==0){
-                Left_Key=e.getKeyCode();
-                count++;
-                return;
-            }
-            if(count==1){
-                Right_Key=e.getKeyCode();
-                count++;
-                return;
-            }
-            if(count==2){
-                Down_Key=e.getKeyCode();
-                count++;
-                return;
-            }
-            if(count==3){
-                Up_Key=e.getKeyCode();
-                count++;
-                return;
-            }
+        /*if(count<4){
+        if(count==0){
+        Left_Key=e.getKeyCode();
+        count++;
+        return;
         }
-        else{
-            if(e.getKeyCode()==Left_Key){
-                //set direction left
-                System.out.println("LEFT");
-                //entity.move(3);
-            }
-            if(e.getKeyCode()==Right_Key){
-                //set direction right
-                System.out.println("RIGHT");
-                //entity.move(1);
-            }
-            if(e.getKeyCode()==Down_Key){
-                //set direction down
-                System.out.println("DOWN");
-                //entity.move(2);
-            }
-            if(e.getKeyCode()==Up_Key){
-                //set direction up
-                System.out.println("UP");
-                //entity.move(0);
-            }
+        if(count==1){
+        Right_Key=e.getKeyCode();
+        count++;
+        return;
+        }
+        if(count==2){
+        Down_Key=e.getKeyCode();
+        count++;
+        return;
+        }
+        if(count==3){
+        Up_Key=e.getKeyCode();
+        count++;
+        return;
+        }
+        }
+        else{*/
+        if(e.getKeyCode()==numpad4){
+            //set direction left
+            System.out.println("LEFT");
+            //entity.move(3);
+        }
+        if(e.getKeyCode()==numpad6){
+            //set direction right
+            System.out.println("RIGHT");
+            //entity.move(1);
+        }
+        if(e.getKeyCode()==numpad2){
+            //set direction down
+            System.out.println("DOWN");
+            //entity.move(2);
+        }
+        if(e.getKeyCode()==numpad8){
+            //set direction up
+            System.out.println("UP");
+            //entity.move(0);
+        }
+        if(e.getKeyCode()==numpad1){
+            //set direction left
+            System.out.println("downLEFT");
+            //entity.move(3);
+        }
+        if(e.getKeyCode()==numpad3){
+            //set direction right
+            System.out.println("downRIGHT");
+            //entity.move(1);
+        }
+        if(e.getKeyCode()==numpad7){
+            //set direction down
+            System.out.println("upleft");
+            //entity.move(2);
+        }
+        if(e.getKeyCode()==numpad9){
+            //set direction up
+            System.out.println("UPRight");
+            //entity.move(0);
         }
     }
 
     //Called when the key is released  
-
     public void keyReleased(KeyEvent e){
-
         //System.out.println("Key Released!!!");
-
         KeyCodeT.setText("Key Code:" + e.getKeyCode());//displays the key code in the text box
-
     }
     //Called when a key is typed
     public void keyTyped(KeyEvent e){
     }
+
     public static void main(String[] args){
         Key1 key = new Key1();
     }
